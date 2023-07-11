34f7bb177650de717840f82c678c0998eba161a4
kyplerdeloitte <klpespiritu@deloitte.is>
2023-07-11 10:48:15 +0000

    inital

---
+++ gatsby-config.js
@@ -0,0 +1,1 @@
+export const plugins = [`gatsby-plugin-vanilla-extract`];
---
+++ src/App.css.ts
@@ -0,0 +1,5 @@
+import { style } from '@vanilla-extract/css'
+
+export const flexCenter = style({
+    background: "red"
+})
